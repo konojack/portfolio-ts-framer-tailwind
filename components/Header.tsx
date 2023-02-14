3adedf97420c26acc886e746ada80cb71f5794ba
konojack <konojack@gmail.com>
2023-02-14 21:24:29 +0100

    creating header section

---
+++ components/Header.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <header className="sticky top-0 max-w-7xl mx-auto p-5 flex flex-wrap items-start justify-between xl:items-center z-20">
+      <div className="flex flex-row items-center">
+        <SocialIcon
+          url="https://facebook.com"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://twitter.com"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://linkedin.com"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </div>
+      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get in touch
+        </p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
