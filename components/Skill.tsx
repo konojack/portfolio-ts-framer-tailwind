f99b70e0b564f24d627c6992f108d89405640f10
konojack <konojack@gmail.com>
2023-02-15 18:26:29 +0100

    skills section

---
+++ components/Skill.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+type Props = {
+  directionLeft?: boolean;
+};
+
+const Skill = ({ directionLeft }: Props) => {
+  return (
+    <div className="group relative flex cursor-pointer">
+      <motion.img
+        initial={{
+          x: directionLeft ? -200 : 200,
+          opacity: 0,
+        }}
+        transition={{
+          duration: 1,
+        }}
+        whileInView={{ x: 0, opacity: 1 }}
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/240px-Unofficial_JavaScript_logo_2.svg.png"
+        className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+      />
+      <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white w-24 h-24 xl:w-32 xl:h-32 rounded-full z-0">
+        <div className="flex items-center justify-center h-full">
+          <p className="text-3xl font-bold text-black opacity-100">100%</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skill;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import About from '@/components/About';
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
+import Skills from '@/components/Skills';
 import WorkExperience from '@/components/WorkExperience';
 import Head from 'next/head';
 
@@ -23,13 +24,14 @@ export default function Home() {
       <section id="about" className="snap-center">
         <About />
       </section>
-      {/* EXPERIENCE */}
 
       <section id="experience" className="snap-center">
         <WorkExperience />
       </section>
 
-      {/* SKILLS */}
+      <section id="skills" className="snap-center">
+        <Skills />
+      </section>
 
       {/* PROJECT */}
 
