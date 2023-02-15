54d9066a2c91037baaa0cbc692d675bb13f1ef30
konojack <konojack@gmail.com>
2023-02-15 15:09:05 +0100

    adding menu buttons inside hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
 
@@ -27,7 +28,7 @@ const Hero = (props: Props) => {
         height="128"
         className="relative rounded-full mx-auto object-cover"
       />
-      <div>
+      <div className="z-20">
         <h2 className="text-sm uppercase text-gray-500 tracking-[10px]">
           Frontend Engineer
         </h2>
@@ -35,6 +36,20 @@ const Hero = (props: Props) => {
           <span className="mr-3">{text}</span>
           <Cursor cursorStyle="_" cursorColor="gray" />
         </h1>
+        <div className="pt-5 z-50">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
